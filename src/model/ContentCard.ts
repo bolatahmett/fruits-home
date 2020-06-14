7445b0676489d335371a4f1c045223f888d2d3f5
Ahmet BOLAT <bolatahmett@gmail.com>
2020-06-15 00:29:28 +0300

    folder names

---
+++ src/model/ContentCard.ts
@@ -0,0 +1,11 @@
+export interface ContentCard {
+    ImageUrl: string;
+    AltInfo: string;
+    Title: string;
+    Description: string;
+    Price: string;
+    StockStatus: string;
+    ProductType: string;
+    ProductCode: string;
+    IsDeleted: string;
+}
